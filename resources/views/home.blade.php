9708ef1224aa0c26124d9e6ca73cc10792896ad4
MarVills <villaflormarbenc@gmail.com>
2022-05-17 21:16:20 +0800

    instagram clone

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scitechinstitute.org/wp-content/uploads/2020/05/freecodecamp.jpg" class="rounded-circle" style="height: 150px;">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>153</strong> posts</div>
+                <div class="pe-5"><strong>23k</strong> followers</div>
+                <div class="pe-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 fw-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/</div>
+            <div class="fw-bold"><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img srcset="https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg" alt="image here" style="height: 400px; width: 390px;">
+        </div>
+        <div class="col-4">
+            <img src="https://i.pinimg.com/originals/85/b7/dc/85b7dce856edeff1f07c060cbde5c369.jpg" alt="image here" style="height: 400px; width: 390px;">
+        </div>
+        <div class="col-4">
+            <img src="https://cdn.britannica.com/99/187399-050-8C81D8D4/cedar-tree-regions-Lebanon-Mediterranean-Sea.jpg" alt="image here" style="height: 400px; width: 390px;">
+        </div>
+    </div>
+</div>
+@endsection
